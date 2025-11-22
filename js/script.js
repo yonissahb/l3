ad26b1701a2fd8b9dcdd265219e4fee3337f3c80
yonissahb <yonissahb@gmail.com>
2025-11-22 17:12:10 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,19 +16,28 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function replaceShortcodes() {
-  const shortcodes = {
-    egt: { name: 'GDTOT', regex: /\[egt id='(.*?)'\]/g, url: 'https://new20.gdtot.dad/file/' },
-  };
+    const shortcodes = {
+        gdf: { name: 'GDFLIX', regex: /\[gdf id='(.*?)'\]/g, url: 'https://new9.gdflix.net/file/'},
+        fpd: { name: 'FILEPRESS', regex: /\[fpd id='(.*?)'\]/g, url: 'https://new1.filepress.cloud/file/'},
+        apd: { name: 'APPDRIVE', regex: /\[apd id='(.*?)'\]/g, url: 'https://appdrive.life/file/'}
+    };
 
-  const containers = document.querySelectorAll('.download-links, .single-links, .zip-links');
+    // Select elements with any of the target classes
+    const contentContainers = document.querySelectorAll('.download-links, .single-links, .zip-links');
 
-  containers.forEach(container => {
-    let content = container.innerHTML;
-    for (const key in shortcodes) {
-      content = content.replace(shortcodes[key].regex, (match, id) => {
-        return `<a href='${shortcodes[key].url}${id}' target='_blank' class="f-links">${shortcodes[key].name}</a>`;
-      });
-    }
-    container.innerHTML = content;
-  });
+    contentContainers.forEach(contentContainer => {
+        let content = contentContainer.innerHTML;
+        for (const key in shortcodes) {
+            if (shortcodes.hasOwnProperty(key)) {
+                content = content.replace(shortcodes[key].regex, (match, id) => {
+                    return `<a href='${shortcodes[key].url}${id}' target='_blank' class="f-links">
+                                ${shortcodes[key].name}
+                            </a>`;
+                });
+            }
+        }
+        contentContainer.innerHTML = content;
+    });
 }
+
+document.addEventListener('DOMContentLoaded', replaceShortcodes);
